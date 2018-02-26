f2c45b235a88b84c9be3862d8e8cccf2d9c022c4
Francisco Roca <frrocaguilar@gmail.com>
2018-02-26 21:46:24 +0100

    changes

--- middleware/check.auth.js
+++ middleware/check.auth.js
@@ -9,7 +9,8 @@ module.exports = (req, res, next) => {
 
             // RETURN ERROR
             return res.status(404).json({
-                message: 'Token errores', 
+                err: err,
+                message: 'Token error', 
                 token: token
             });
 
